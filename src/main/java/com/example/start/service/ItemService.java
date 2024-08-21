6bee0a1857c933dcac6f63865bb25a96f9189462
iamseung <seesungseok@naver.com>
2024-08-21 23:08:28 +0900

    프로젝트 기초 셋업
    - Entity, Repository, Service, Controller 뼈대 생성

---
+++ src/main/java/com/example/start/service/ItemService.java
@@ -0,0 +1,20 @@
+package com.example.start.service;
+
+import com.example.start.dto.request.CreateItemRequest;
+import com.example.start.entity.Item;
+import com.example.start.repository.ItemRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class ItemService {
+    private final ItemRepository itemRepository;
+
+    public void createItem(CreateItemRequest request) {
+        Item item = new Item();
+        itemRepository.save(item);
+    }
+}
